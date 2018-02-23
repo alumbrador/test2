796f9fbc9ba7abd8da4602fcb8d90811f2191d93
alumbrador <muradnm@code.edu.az>
2018-02-23 20:31:23 +0400

    project

---
+++ RETURN APP/RETURN APP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RETURN_APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = 5;
+            Student Hesabla()
+            {
+                return new Student("Murad");
+
+            }
+            Console.WriteLine(Hesabla().Surname);
+        
+        }
+    }
+    class Student
+    {
+        
+        public string Surname;
+    
+
+        public Student(string _surname)
+        {
+            
+            Surname = _surname;
+
+        }
+    }
+}
